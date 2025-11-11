297f47605a21bf2080f09b85e75fccfbddf7d27c
Oluwafemi Oloyede <oloyedeoluwafemi22@gmail.com>
2025-11-11 17:51:00 +0100

    set up postgres db

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,9 +1,9 @@
-from .models import User
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     """
